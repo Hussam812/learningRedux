8d8c94c2b27091ae2f665dc0b621da2aa1690df2
Hussam812 <hossamhosnihussien@gmail.com>
2023-09-28 11:58:24 +0200

    action creaator

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 console.log(Redux);
 
-const action = {
-  type: "WITHDRAW_MONEY",
-};
-const action2 = {
-  type: "DEPOST_MONEY",
-};
+const withdraw = (amount) => ({ type: "WITHDRAW_MONEY", payload: amount });
+const diposite = (amount) => ({ type: "DEPOST_MONEY", payload: amount });
 const reducer = (state = 1000, action) => {
   switch (action.type) {
     case "WITHDRAW_MONEY":
-      return state - 100;
+      return state - action.payload;
     case "DEPOST_MONEY":
-      return state + 100;
+      return state + action.payload;
     default:
       return state;
   }
@@ -22,16 +18,17 @@ console.log(store.getState());
 
 const depbtn = document.getElementById("deposite");
 depbtn.addEventListener("click", () => {
-  store.dispatch(action2);
+  store.dispatch(diposite(100));
 });
 
 const withdebtn = document.getElementById("Withdraw");
 withdebtn.addEventListener("click", () => {
-  store.dispatch(action);
+  store.dispatch(withdraw(200));
 });
+const viewState = document.getElementById("state");
+viewState.innerHTML = `State:  ${store.getState()}`;
 
 store.subscribe(() => {
-  const viewState = document.getElementById("state");
   viewState.innerHTML = `State:  ${store.getState()}`;
   console.log(store.getState());
 
