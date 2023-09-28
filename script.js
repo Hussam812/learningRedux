d1e01ecb394a4603c48bd20784183ff523cba02b
Hussam812 <hossamhosnihussien@gmail.com>
2023-09-28 15:24:17 +0200

    combine Reducer

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ console.log(Redux);
 
 const withdraw = (amount) => ({ type: "WITHDRAW_MONEY", payload: amount });
 const diposite = (amount) => ({ type: "DEPOST_MONEY", payload: amount });
-const reducer = (state = 1000, action) => {
+const addProduct = (payload) => ({ type: "AddProduct", payload });
+const bankReducer = (state = 1000, action) => {
   switch (action.type) {
     case "WITHDRAW_MONEY":
       return state - action.payload;
@@ -12,10 +13,23 @@ const reducer = (state = 1000, action) => {
       return state;
   }
 };
+const productReducer = (state = [], action) => {
+  switch (action.type) {
+    case "AddProduct":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+//console.log(store.getState());
 
-const store = Redux.createStore(reducer);
-console.log(store.getState());
+const appReducer = Redux.combineReducers({
+  bank: bankReducer,
+  product: productReducer,
+});
 
+const store = Redux.createStore(appReducer);
+//console.log(store.getState().bank);
 const depbtn = document.getElementById("deposite");
 depbtn.addEventListener("click", () => {
   store.dispatch(diposite(100));
@@ -23,13 +37,13 @@ depbtn.addEventListener("click", () => {
 
 const withdebtn = document.getElementById("Withdraw");
 withdebtn.addEventListener("click", () => {
-  store.dispatch(withdraw(200));
+  store.dispatch(withdraw(100));
 });
 const viewState = document.getElementById("state");
-viewState.innerHTML = `State:  ${store.getState()}`;
+viewState.innerHTML = `State:  ${store.getState().bank}`;
 
 store.subscribe(() => {
-  viewState.innerHTML = `State:  ${store.getState()}`;
+  viewState.innerHTML = `State:  ${store.getState().bank}`;
   console.log(store.getState());
 
   console.log("Current State: ", store.getState());
